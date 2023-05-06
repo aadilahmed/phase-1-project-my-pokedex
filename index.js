c4198c7b68501b86aef0b8cbdf9044b8413c714b
Aadil Ahmed <aadilahmed0@gmail.com>
2023-05-06 02:44:42 -0700

    adds 10 pokemon elements to DOM

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
-.then(response => response.json())
-.then(pokemonContainerList => {
-    return fetch(pokemonContainerList.results[0].url);
-})
-.then(response => response.json())
-.then(pokemonObj => {
-    console.log(pokemonObj.height);
-});
+let promises = initializeFetchArray();
+
+Promise.all(promises)
+.then(results => results.forEach(pokemonObj => {
+    console.log(pokemonObj.name); 
+     let pokemon = {
+        name:pokemonObj.name,
+        image:pokemonObj.sprites.other["official-artwork"].front_default,
+        caught:false,
+        nickname:""
+    }
+
+    let card = document.createElement('div');
+    card.className = 'card';
+    let pokeName = document.createElement('h2');
+    pokeName.textContent = pokemon.name;
+    let pokeNickname = document.createElement('p');
+    pokeNickname.textContent = pokemon.nickname;
+    let pokeImage = document.createElement('img');
+    pokeImage.className = 'pokemon-image'
+    pokeImage.src = pokemon.image;
+
+    card.append(pokeName);
+    card.append(pokeNickname);
+    card.append(pokeImage);
+
+    document.querySelector("#pokemon-collection").appendChild(card); 
+ })
+); 
+
+function initializeFetchArray(){
+    let promises = [];
+    for(let i = 0; i < 10; i++) {
+        promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i+1}`)
+        .then(response => response.json()));
+    }
+
+    return promises;
+}
