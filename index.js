b87b72b582b10b88c11a82a52f5127a45ef2e39e
Aadil Ahmed <aadilahmed0@gmail.com>
2023-05-08 15:45:51 -0700

    adds nickname creation functionality

--- index.js
+++ index.js
@@ -48,8 +48,11 @@ function renderCard(pokemon){
     let caughtBtn = document.createElement('button');
     caughtBtn.className = 'caught-btn';
     caughtBtn.innerText = ' Add to Pokédex '; 
+    
+    let container = createNickname(pokeNickname);
 
     card.append(pokeName);
+    card.append(container);
     card.append(pokeNickname);
     card.append(pokeImage); 
     card.append(caughtBtn);
@@ -61,4 +64,53 @@ function renderCard(pokemon){
     }); 
 
     return card;
+}
+
+function createNickname(pokeNickname) {
+    let container = document.createElement('div');
+    container.className = 'container';
+
+    let form = document.createElement('form');
+    form.className = 'add-nickname-form';
+
+    let hyperlink = document.createElement('a');
+    hyperlink.className = 'add-nickname';
+    hyperlink.setAttribute('href',"javacsript:void(0);");
+    hyperlink.innerText = 'Add nickname'
+    hyperlink.style.display = 'block'
+
+    let textbox = document.createElement('input');
+    textbox.setAttribute("type", "text");
+    textbox.className = 'input-text';
+    textbox.name = 'nickname';
+    textbox.value = ''; 
+
+    let submit = document.createElement('input');
+    submit.setAttribute("type", "submit");
+    submit.className = 'submit';
+    submit.name = 'submit';
+    submit.value = 'Save'; 
+    
+    form.appendChild(textbox);
+    form.appendChild(submit);
+
+    form.style.display = 'none';
+
+    hyperlink.addEventListener('click', (e) => {
+        hyperlink.style.display = 'none';
+        form.style.display = 'block'
+    })   
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        pokeNickname.textContent = e.target.nickname.value;
+        form.style.display = 'none';      
+        pokeNickname.style.display = 'block';        
+    })
+
+    container.append(form);
+    container.append(hyperlink);  
+    
+    return container;
 }
